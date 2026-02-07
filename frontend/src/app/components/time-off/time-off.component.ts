cb8259663ace5cd2a0dec75e04d24f7d8a7d876b
codybreitling <codybreitling@gmail.com>
2026-02-07 14:27:15 -0600

    Add date input validation

--- frontend/src/app/components/time-off/time-off.component.ts
+++ frontend/src/app/components/time-off/time-off.component.ts
@@ -9,6 +9,23 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './time-off.component.css'
 })
 export class TimeOffComponent {
+
   today = new Date().toISOString().split('T')[0];
-  startDate = this.today;
+
+  startDate: string | null = null;
+  endDate: string | null = null;
+
+  onStartChange() {
+    // If end date is before start date, fix it
+    if (this.endDate && this.endDate < this.startDate!) {
+      this.endDate = this.startDate;
+    }
+  }
+
+  onEndChange() {
+    // If start date is after end date, fix it
+    if (this.startDate && this.startDate > this.endDate!) {
+      this.startDate = this.endDate;
+    }
+  }
 }
