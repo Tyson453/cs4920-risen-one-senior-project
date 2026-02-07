46eeeb4d23d2c9c75f29d5e01e6b4cb6a2a284b4
Tyson453 <tyson112305@gmail.com>
2026-02-06 18:33:15 -0600

    Initial admin page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,11 +1,12 @@
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './components/admin/admin.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { DailyStatusComponent } from './components/daily-status/daily-status.component';
 import { TeamSummaryComponent } from './components/team-summary/team-summary.component';
 import { TimeOffComponent } from './components/time-off/time-off.component';
 import { CertificationTrainingComponent } from './components/certification-training/certification-training.component';
-import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
@@ -14,7 +15,8 @@ export const routes: Routes = [
   { path: 'team-summary', component: TeamSummaryComponent },
   { path: 'daily-status', component: DailyStatusComponent },
   { path: 'time-off', component: TimeOffComponent },
-    { path: 'certification-training', component: CertificationTrainingComponent },
+  { path: 'admin', component: AdminComponent },
+  { path: 'certification-training', component: CertificationTrainingComponent },
 ];
 
 @NgModule({
---
+++ frontend/src/app/components/admin/admin.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin',
+  standalone: false,
+  templateUrl: './admin.component.html',
+  styleUrls: ['./admin.component.scss'],
+})
+export class AdminComponent implements OnInit {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+}
