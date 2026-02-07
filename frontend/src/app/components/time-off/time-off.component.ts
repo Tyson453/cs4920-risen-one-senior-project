410efbf4d414a8cece8bf85dd3de1e6973478db0
sandeepreddy-0444 <sandeepjcg@gmail.com>
2026-02-06 22:57:35 -0600

    Add Time Off page UI

---
+++ frontend/src/app/components/time-off/time-off.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-time-off',
+  standalone: true,
+  imports: [],
+  templateUrl: './time-off.component.html',
+  styleUrl: './time-off.component.css'
+})
+export class TimeOffComponent {
+
+}
