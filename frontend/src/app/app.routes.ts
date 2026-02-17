46ebada6e558b0db94a04434a5ebbb0512fe9d23
sandeepreddy-0444 <sandeepjcg@gmail.com>
2026-02-16 19:15:48 -0600

    Add Projects page UI and routing

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,26 +1,20 @@
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
-import { NgModel } from '@angular/forms';
-import { NgModule } from '@angular/core';
 import { DailyStatusComponent } from './components/daily-status/daily-status.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent }, 
-  {
-    path: 'daily-status',
-    component: DailyStatusComponent
-  },
-  ];
+  { path: 'home', component: HomeComponent },
+  { path: 'daily-status', component: DailyStatusComponent },
+  { path: 'projects', component: ProjectsComponent }
+];
 
-  @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule],
-    providers: []
-  })
-
-  export class AppRoutingModule{
-   
-  }
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
