1d3d7502b7aafd29d0fb9c61651a49f97ddc9181
alexhtripp <alexhtripp@gmail.com>
2026-04-19 19:10:06 -0500

    removed notification references

--- frontend/src/app/components/notifications/notifications.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-notifications',
-  templateUrl: './notifications.component.html',
-  styleUrl: './notifications.component.css',
-  standalone: false
-})
-export class NotificationsComponent {}
